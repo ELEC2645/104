756c94670e66d2f0c2165f16a84ddf91e81eb1ef
eencae <3027785+eencae@users.noreply.github.com>
2018-09-05 16:11:51 +0100

    Formatting changes

--- memory/main.cpp
+++ memory/main.cpp
@@ -3,6 +3,7 @@
 int main() {
   // create a 32-bit (4 byte) variable
   unsigned int variable = 0xABCDEF89;
+  std::cout << std::uppercase;  // print hex values in uppercase
   std::cout << "Variable value is 0x" << std::hex << variable << std::endl;
   std::cout << "Size of variable is " << sizeof(variable) << " bytes\n";
   std::cout << "Address of variable is " << &variable << std::endl;
@@ -12,11 +13,13 @@ int main() {
   unsigned char *pointer = (unsigned char *)&variable;
 
   // iterate through each byte in the 4-byte integer variable
+  std::cout << "-----------------------\n";
   for (int i = 0; i < 4; i++) {
     // now print out the byte at that address address
-    std::cout << (unsigned int *)pointer << " | ";
-    std::cout << std::hex << (unsigned int)*pointer << std::endl;
+    std::cout << "| " << (unsigned int *)pointer << " | ";
+    std::cout << (unsigned int)*pointer << " |\n";
     // increment the pointer to point at the next address
     pointer++;
   }
+  std::cout << "-----------------------\n";
 }
